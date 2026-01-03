8008198180064ec14a30a442f2882fce88ce09df
Yasin Husen <yhusen636@gmail.com>
2026-01-03 18:20:09 +0300

    Fix NavLink.jsx syntax error

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,11 +1,14 @@
-import { NavLink as RouterNavLink } from "react-router-dom";
+import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
 const NavLink = forwardRef(
   ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
     return (
-      
+      <RouterNavLink
+        ref={ref}
+        to={to}
+        className={({ isActive, isPending }) =>
           cn(
             className,
             isActive && activeClassName,
@@ -21,4 +24,3 @@ const NavLink = forwardRef(
 NavLink.displayName = "NavLink";
 
 export { NavLink };
-
